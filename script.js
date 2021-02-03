d6b037e2361cfa85400c04dda83523de2bd6d325
Giedre Klimaite <giedre.klima@gmail.com>
2021-02-03 21:54:02 +0200

    some adds

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
 var quizNumEl = document.getElementById("quiz-num");
 var numLengthEl = document.getElementById("num-length");
 var inputNumEl = document.getElementById("inputMain");
-
 var submitMainEl = document.getElementById("submitMain");
-
-function generateNum(n = 4) {
+var resultEl = document.getElementById("result");
+// main quiz logic
+var n = 4;
+function generateNum(n) {
   let sum = "";
   for (let i = 0; i < n; i++) {
     var skRand = Math.floor(Math.random() * 10);
     sum += skRand;
   }
-  return sum;
+  quizNumEl.innerHTML = sum;
+  setTimeout(hide, (n * 1000) / 2);
 }
 numLengthEl.addEventListener("keyup", (e) => {
-  if (e.keyCode === 13) {
+  if (e.keyCode === 13 && numLengthEl.value == "") {
     e.preventDefault();
-    var num = generateNum(numLengthEl.value);
-    quizNumEl.innerHTML = num;
+    generateNum(n);
+  } else if (e.keyCode === 13 && numLengthEl.value != "") {
+    e.preventDefault();
+    n = numLengthEl.value;
+    generateNum(n);
   }
 });
 
 function check() {
-  var checkText =
-    quizNumEl.textContent == inputNumEl.value
-      ? `<i class="fas fa-check-circle green"></i>`
-      : `<i class="fas fa-times-circle red"></i>`;
+  var checkText = "";
+  if (quizNumEl.textContent == inputNumEl.value) {
+    checkText = `<i class="fas fa-check-circle green"></i>`;
+    generateNum(++n);
+  } else {
+    checkText = `<i class="fas fa-times-circle red"></i>`;
+    generateNum(--n);
+  }
 
   document.getElementById("check").innerHTML = checkText;
-  return checkText;
-}
-
-function numLengthDependency() {
-  if (true) {
-  }
 }
 
 inputMain.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
     check();
+    inputNumEl.value = "";
+    quizNumEl.style.color = "black";
   }
 });
 submitMain.addEventListener("click", (e) => {
   e.preventDefault();
   check();
+  inputNumEl.value = "";
+  quizNumEl.style.color = "black";
 });
+// additional quiz "features"
+function hide() {
+  quizNumEl.style.color = "white";
+}
